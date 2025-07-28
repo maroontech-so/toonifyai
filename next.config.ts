56ea9acef86a8c13bec224f0ff26f53b246a7872
maroontech-so <micahprince60@gmail.com>
2025-07-28 02:21:14 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type {NextConfig} from 'next';
 
+const isGithubPages = process.env.NODE_ENV === 'production'
+
+const repo = 'toonifyai' // 👈 your GitHub repo name
+
+export default {
+  output: 'export',
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
